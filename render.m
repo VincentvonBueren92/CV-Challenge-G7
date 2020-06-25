21ff243dd758bc4a5c1b0d215fea403769204b11
Johannes-Ernst <67051587+Johannes-Ernst@users.noreply.github.com>
2020-06-25 09:16:10 +0200

    First implementation

--- render.m
+++ render.m
@@ -2,5 +2,72 @@ function [result] = render(frame,mask,bg,mode)
   % Add function description here
   %
   %
+  switch mode
+      case 'foreground'
+          % mask original frame
+          result(:,:,1) = frame(:,:,1).*uint8(mask);
+          result(:,:,2) = frame(:,:,2).*uint8(mask);
+          result(:,:,3) = frame(:,:,3).*uint8(mask);
+     
+      case 'background'
+        %get background mask
+        mask_complement = imcomplement(mask);
+        
+        %mask original frame
+        result(:,:,1) = frame(:,:,1).*uint8(mask_complement);
+        result(:,:,2) = frame(:,:,2).*uint8(mask_complement);
+        result(:,:,3) = frame(:,:,3).*uint8(mask_complement);
+      
+      case 'overlay'
+        % get background mask
+        mask_complement = imcomplement(mask);
+
+        % get green Image
+        fg_green = zeros(size(frame));
+        % green foreground
+        fg_green(:,:,2) = 255*uint8(mask);
+        
+        % get red Image
+        bg_red = zeros(size(frame));
+        % red background
+        bg_red(:,:,1) = 255*uint8(mask_complement);
+
+        % combine red and green
+        overlay(:,:,1) = fg_green(:,:,1)+bg_red(:,:,1);
+        overlay(:,:,2) = fg_green(:,:,2)+bg_red(:,:,2);
+        overlay(:,:,3) = fg_green(:,:,3)+bg_red(:,:,3);
+
+        % make transparent overlay
+        result(:,:,1) = uint8(overlay(:,:,1))*0.5+frame(:,:,1);
+        result(:,:,2) = uint8(overlay(:,:,2))*0.5+frame(:,:,2);
+        result(:,:,3) = uint8(overlay(:,:,3))*0.5+frame(:,:,3);
+      
+      case 'substitute'
+          
+        % scale background to frame size
+        scaled_bg = imresize(bg,[size(frame,1) size(frame,2)]);
+        
+        % get background mask
+        mask_complement = imcomplement(mask);
+
+        % mask foreground
+        masked_Im(:,:,1) = frame(:,:,1).*uint8(mask);
+        masked_Im(:,:,2) = frame(:,:,2).*uint8(mask);
+        masked_Im(:,:,3) = frame(:,:,3).*uint8(mask);
+
+        % mask background
+        comp_masked_bg(:,:,1) = scaled_bg(:,:,1).*uint8(mask_complement);
+        comp_masked_bg(:,:,2) = scaled_bg(:,:,2).*uint8(mask_complement);
+        comp_masked_bg(:,:,3) = scaled_bg(:,:,3).*uint8(mask_complement);
+
+        % combine fore- and background
+        result(:,:,1) = comp_masked_bg(:,:,1)+masked_Im(:,:,1);
+        result(:,:,2) = comp_masked_bg(:,:,2)+masked_Im(:,:,2);
+        result(:,:,3) = comp_masked_bg(:,:,3)+masked_Im(:,:,3);
+      
+      otherwise 
+        disp('no valid mode')
+  end
+  
 
 end
--- segmentation.m
+++ segmentation.m
@@ -3,5 +3,95 @@ function [mask] = segmentation(left,right)
   %
   %
 
+%% get gray Images
+  for i = 1:size(left)
+        img_double = double(left{i});
+        gray_image_double = img_double(:,:,1)*0.299 + img_double(:,:,2)*0.587 + img_double(:,:,3)*0.114;
+        gray_image{i} = uint8(gray_image_double);
+    end
 
+%% get Image difference of the last 3 frames (r,g,b seperatly)
+    im_diff_r{1} = left{1}(:,:,1)-left{2}(:,:,1);
+    im_bin_r{1} = imbinarize(im_diff_r{1});
+
+    im_diff_g{1} = left{1}(:,:,2)-left{2}(:,:,2);
+    im_bin_g{1} = imbinarize(im_diff_g{1});
+
+    im_diff_b{1} = left{1}(:,:,3)-left{2}(:,:,3);
+    im_bin_b{1} = imbinarize(im_diff_b{1});
+   
+    im_diff_r{2} = left{2}(:,:,1)-left{3}(:,:,1);
+    im_bin_r{2} = imbinarize(im_diff_r{2});
+
+    im_diff_g{2} = left{2}(:,:,2)-left{3}(:,:,2);
+    im_bin_g{2} = imbinarize(im_diff_g{2});
+
+    im_diff_b{2} = left{2}(:,:,3)-left{3}(:,:,3);
+    im_bin_b{2} = imbinarize(im_diff_b{2});
+
+    % compose all detected difference
+    im_bin_1 = im_bin_r{1} | im_bin_g{1} |im_bin_b{1};
+    im_bin_2 = im_bin_r{2} | im_bin_g{2} |im_bin_b{2};
+
+    im_bin = im_bin_1 | im_bin_2;
+    
+%% fill difference map
+    %TODO: this process is currently a quite random sequence of functions
+    %to fill out the difference map -> find more deterministic way
+
+    BW = bwmorph(im_bin,'bridge'); %try too conect components
+    BW = bwmorph(BW,'thicken',10); % thicken components
+    
+    se = strel('line', 5, 90); % Structuring element for dilation
+    BW = imdilate(BW, se); % Dilating the image
+    se = strel('line', 5, 0); % Structuring element for dilation
+    BW = imdilate(BW, se); % Dilating the image
+    
+    BW = imfill(BW,'holes');
+    
+    se = strel('line', 4, 90); % Structuring element for dilation
+    BW = imdilate(BW, se); % Dilating the image
+    se = strel('line', 4, 0); % Structuring element for dilation
+    BW = imdilate(BW, se); % Dilating the image
+    
+    BW = imfill(BW,'holes');
+
+    BW_difference = BW;
+%% get intensity gradiant
+    edges = edge(gray_image{1},'Canny');
+    %TODO: idea(Hannes & Vincent) smooth gradient by applying gussian
+    %filter
+    
+%% AND - Connection between  difference map and gradient edges
+    AND = BW_difference & edges;
+    
+    
+%% fill MASK  
+    %TODO: this process is currently a quite random sequence of functions
+    %to fill out the difference map -> find more deterministic way
+    se = strel('line', 3, 90); % Structuring element for dilation
+    mask1 = imdilate(AND, se); % Dilating the image
+    
+    se = strel('line', 3, 0); % Structuring element for dilation
+    mask1 = imdilate(mask1, se); % Dilating the image
+    mask2 = imfill(mask1,'holes');
+
+    %only take the biggest component
+    mask = bwareafilt(mask2, 1, 'Largest');
+    % TODO find alternative for more than one person
+
+% plot steps in between
+    figure(1);
+    subplot(2,3,1);
+    imshow(im_bin);
+    subplot(2,3,2);
+    imshow(BW_difference);
+    subplot(2,3,3);
+    imshow(AND);
+    subplot(2,3,4);
+    imshow(mask2);
+    subplot(2,3,5);
+    imshow(mask);
+
+ 
 end
