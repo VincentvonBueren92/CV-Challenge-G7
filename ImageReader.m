63e2ed51bb311fca8d9483ebe41107e41aaa88cc
Hannes Burrichter <hannes.burrichter@tum.de>
2020-07-07 13:42:39 +0200

    Added a lot of comments. Formated the code. Changed the variable names to be more intuitive. Updated the input parser. Added os independent code for getting the image names in the folders.

--- ImageReader.m
+++ ImageReader.m
@@ -1,13 +1,27 @@
+
+% Image reader class
+% 
+% Computer Vision challenge 2020 (Group 7)
+%
+% Input:  (str)  src       Path to the scene (e.g. '/Users/hannes/ChokePoint_Dataset/P1E_S1')          No default value 
+%         (int)  L         Chosen camera for the left frame (Valid: 1 or 2)                            Default: 1
+%         (int)  R         Chosen camera for the left frame (Valid: 2 or 3)                            Default: 2
+%         (int)  start     Start frame in the scene                                                    Default: 0
+%         (int)  N         Get the the current frame + N further frames back (N+1 frames in total)     Default: 0
+% 
+% Output: (int)  left      Tensor with the images of the left camera. Dimensions: 600 × 800 × (N + 1) × 3 
+%         (int)  right     Tensor with the images of the left camera. Dimensions: 600 × 800 × (N + 1) × 3
+%         (bool) loop      FALSE if there are still enough images to return a full tensor.
+%                          TRUE  if the user tries to grab more imagines than left in the scene_folder. Jump back to start value in the next call of next()
+
 classdef ImageReader < handle
-  % Add class description here
-  %
-  %
+    
     properties
-        src = '';
-        L = 0;
-        R = 0;
-        start = 0;
-        N = 0;
+        src = '';       % Path to the scene (e.g., '/Users/hannes/ChokePoint_Dataset/P1E_S1'
+        L = 1;          % Chosen camera for the left frame (Valid: 1 or 2)
+        R = 2;          % Chosen camera for the left frame (Valid: 2 or 3)
+        start = 0;      % Start frame in the scene
+        N = 0;          % Get the the current frame + N further frames back (N+1 frames in total)
     end
     
     methods
@@ -16,23 +30,24 @@ classdef ImageReader < handle
       
             % Input parser
             
-            % Standart values
-            L_df = 1;
-            R_df = 2;
+            % Default values
             start_df = 0;
-            N_df = 1;
+            N_df = 0;
 
             % Requirements
-            L_val = @(x) isnumeric(x) && (x >= 1) && (x <= 2);
-            R_val = @(x) isnumeric(x) && (x >= 2) && (x <= 3);
+            src_val = @(x) exist(src, 'dir') == 7;
+            L_val = @(x) isnumeric(x) && (ismember(x, [1, 2]));
+            R_val = @(x) isnumeric(x) && (ismember(x, [2, 3]));
             start_val = @(x) isnumeric(x) && (x >= 0);
             N_val = @(x) isnumeric(x) && (x >= 0);
 
             % Matlab parser function
             p = inputParser;
-            addRequired(p, 'src');
-            addRequired(p, 'L');
-            addRequired(p, 'R');
+            
+            % Check requirements
+            addRequired(p, 'src', src_val);
+            addRequired(p, 'L', L_val);
+            addRequired(p, 'R', R_val);
             addOptional(p, 'start', start_df, start_val);
             addOptional(p, 'N', N_df, N_val);
 
@@ -45,9 +60,9 @@ classdef ImageReader < handle
             ir.N = p.Results.N;
         end
         
-        function [left, right, loop] = next(this)
+        function [loop] = next(this)
             
-            % persistent loop variable
+            % Persistent second loop variable. Will be updated to the actual 'loop' variable in the end
             persistent l;
             
             % Init l
@@ -55,68 +70,80 @@ classdef ImageReader < handle
                 l = 0;
             end           
             
+            % Get scene folder name (e.g. 'P1E_S1') from the splitted up src path
             path_splits = regexp(this.src, filesep, 'split');
-            folder = char(path_splits(end));    % Scene folder
+            scene_folder = char(path_splits(end)); 
             
-            left_folder = [folder, '_C', num2str(this.L)];    % Camera folder inside scene folder
-            right_folder = [folder, '_C', num2str(this.R)];
+            % Left/Right camera folder inside the scene folder
+            cam_l_folder  = [scene_folder, '_C', num2str(this.L)];
+            cam_r_folder  = [scene_folder, '_C', num2str(this.R)];
             
-            left_path = fullfile(this.src, left_folder);       % Path of scene folder + specific camera folder
-            right_path = fullfile(this.src, right_folder);
+            % Path of scene folder + left/right camera folder
+            scene_cam_l_path = fullfile(this.src, cam_l_folder);
+            scene_cam_r_path = fullfile(this.src, cam_r_folder);
             
-            if ismac
-                left_image_names = dir([left_path '/*.jpg']);      % Image names
-                right_image_names = dir([left_path '/*.jpg']);
-                % Code to run on Mac platform
-            elseif ispc
-                disp("ad");
-                left_image_names = dir([left_path '\*.jpg'])          % Image names
-                right_image_names = dir([left_path '\*.jpg']);
-              
-                % Code to run on Windows platform
-            else
-                disp('Platform not supported')
-            end
+            % Image names of left/right camera
+            img_list_l = dir(fullfile(scene_cam_l_path,'*.jpg'));
+            img_list_r = dir(fullfile(scene_cam_r_path,'*.jpg'));
+            
+%             if ismac
+%                 img_list_l = dir([scene_cam_l_path '/*.jpg']);      % Image names
+%                 img_list_r = dir([scene_cam_l_path '/*.jpg']);
+%                 % Code to run on Mac platform
+%             elseif ispc
+%                 disp("ad");
+%                 img_list_l = dir([scene_cam_l_path '\*.jpg']) ;         % Image names
+%                 img_list_r = dir([scene_cam_l_path '\*.jpg']);
+%               
+%                 % Code to run on Windows platform
+%             else
+%                 disp('Platform not supported')
+%             end
              
             
-            % Tensors with the images
+            % Tensors with the images of the left/right camera. Dimensions: 600 × 800 × (N + 1) × 3
             left = [];
             right = [];
             
+            % Calculation of the start and end value of the loop in which the images will be read
             loop_start = 1 + this.start;
             loop_end = 1 + this.start + this.N;
-            img_num = length(left_image_names);
+            img_num = length(img_list_l);
             
-            % Start at the beginning of the function if the loop variable was set to 1 in the last call
+            % Start at the beginning of the image folder if the loop variable was set to 1 in the last call
             if l == 1
                 loop_start = 1;
                 loop_end = loop_start + this.N;
                 l = 0;
             end
             
-            % Set loop variable if there are not enough images
+            % Check if there are not enough images left
             if loop_end >= img_num
                 loop_end = img_num;
                 l = 1;
+                disp('End of scene_folder reached. Jumping back to the set start value in the next function call');
             end
            
             % Read images
             for k = loop_start : loop_end
-                left_name = fullfile(left_path, left_image_names(k).name);  % Full path of an image                
-                left_img = imread(left_name);
-                left_img = reshape(left_img, size(left_img,1), size(left_img,2), 1, 3);   % Change dimensions
-                left = cat(3, left, left_img);                  % Create tensor with mutiple images (600 × 800 × (N + 1) · 3)
+                % Create full path of the current left/right image 
+                img_name_l = fullfile(scene_cam_l_path, img_list_l(k).name)             
+                img_name_r = fullfile(scene_cam_r_path, img_list_r(k).name);
                 
-                right_name = fullfile(right_path, right_image_names(k).name);  % Full path of an image
-                right_img = imread(right_name);
-                right_img = reshape(right_img, size(right_img,1), size(left_img,2), 1, 3);
-                right = cat(3, right, right_img);
+                % Read left/right image from path
+                img_l = imread(img_name_l);                                   
+                img_r = imread(img_name_r);
+                
+                % Change dimensions of the images (600 × 800 × 3) to (600 × 800 × 1 × 3)
+                img_l = reshape(img_l, size(img_l,1), size(img_l,2), 1, 3);   
+                img_r = reshape(img_r, size(img_r,1), size(img_l,2), 1, 3);
+                
+                % Add the latest images to the two tensors (600 × 800 × (N + 1) × 3)
+                left = cat(3, left, img_l);                  
+                right = cat(3, right, img_r);
             end
             
-            % figure;
-            % image(left(:,:,1,1));
-            size(left); 
-            
+            % Update the loop variable
             loop = l;
 
         end
